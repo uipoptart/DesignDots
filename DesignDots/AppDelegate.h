4cc1a007b553b1e68922782ccede13422d5fae45
Nat Osten <uipoptart@gmail.com>
2014-06-17 17:18:41 -0700

    Initial Commit

---
+++ DesignDots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignDots
+//
+//  Created by Nat Osten on 6/17/14.
+//  Copyright (c) 2014 Nat Osten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignDots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignDots
+//
+//  Created by Nat Osten on 6/17/14.
+//  Copyright (c) 2014 Nat Osten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
