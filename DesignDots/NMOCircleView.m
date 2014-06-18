ec578d3b0f2f3e7517702866f897fe5a57e558c5
Nat Osten <uipoptart@gmail.com>
2014-06-18 14:11:42 -0700

    BUG FIX: Code Cleanup

--- DesignDots/NMOCircleView.m
+++ DesignDots/NMOCircleView.m
@@ -19,30 +19,25 @@
 {
     [super layoutSubviews];
     
+    CGRect bounds = [self.layer bounds];
+    CGFloat lineWidth = self.lineWidth;
     CAShapeLayer *circleLayer = [CAShapeLayer layer];
-    [circleLayer setBounds:[self.layer bounds]];
+    [circleLayer setBounds:bounds];
     [self.layer addSublayer:circleLayer];
    
-    CGRect bounds = [self.layer bounds];
     CGRect ovalRect = CGRectOffset(bounds, bounds.size.width / 2, bounds.size.height / 2);
-    ovalRect = CGRectInset(ovalRect, self.lineWidth / 2, self.lineWidth / 2);
+    ovalRect = CGRectInset(ovalRect, lineWidth / 2, lineWidth / 2);
     UIBezierPath *circle = [UIBezierPath bezierPathWithOvalInRect:ovalRect];
    
     [circleLayer setPath:circle.CGPath];
     [circleLayer setFillColor:[self.fillColor CGColor]];
     [circleLayer setStrokeColor:[self.strokeColor CGColor]];
-    [circleLayer setLineWidth:self.lineWidth];
+    [circleLayer setLineWidth:lineWidth];
 }
 
 - (CGSize)intrinsicContentSize
 {
     return self.size;
 }
-
-//- (UIEdgeInsets)alignmentRectInsets
-//{
-//    CGFloat lineWidth = -self.lineWidth;
-//    return UIEdgeInsetsMake(lineWidth, lineWidth, lineWidth, lineWidth);
-//}
 
 @end
